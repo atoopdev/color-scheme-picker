503a68b89ad7c1afd36bb801e2064b0c00357dd9
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-21 08:55:20 -0700

    grab submitted form data

--- colorschemegen.js
+++ colorschemegen.js
@@ -1,12 +1,16 @@
 const formEL = document.getElementById("color-picker-form")
-
+let colorSelection = {}
 // ------------------- grab form data -------------------------------
 
 formEL.addEventListener("submit", event =>{
     event.preventDefault()
     console.log("Sumbit button clicked")
     const ourFormData = new FormData(event.target)
-    const color = ourFormData.get("color")
-    const colorScheme = ourFormData.get("colorScheme")
-    console.log(`Form color data: ${color} ${colorScheme}`) 
+    const colorChoice = ourFormData.get("color")
+    const colorSchemeChoice = ourFormData.get("colorScheme")
+    colorSelection = {
+        color: colorChoice,
+        colorScheme: colorSchemeChoice
+    }
+    console.log(`Form color data: ${colorSelection.color} ${colorSelection.colorScheme}`) 
 })
